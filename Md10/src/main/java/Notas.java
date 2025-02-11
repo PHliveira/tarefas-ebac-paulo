c0439b4fce1b056f44d4a4dc19b67562d11386a9
PHliveira <paulo.criar@gmail.com>
2025-02-10 22:46:49 -0300

    "Atualizaçao exercicio"

--- Md10/src/main/java/Notas.java
+++ Md10/src/main/java/Notas.java
@@ -1,23 +1,39 @@
+import jdk.jshell.execution.Util;
+
+import javax.swing.text.Utilities;
+import java.util.Scanner;
+
 public class Notas {
 
     public static void main (String args[]) {
 
 
         System.out.println("Classificação para prova");
+        System.out.println("Digite 4 notas para a sua Média :");
+        Scanner s = new Scanner(System.in);
+        Integer n1 = s.nextInt();
+        Integer n2 = s.nextInt();
+        Integer n3 = s.nextInt();
+        Integer n4 = s.nextInt();
 
-        for (int i=0 ; i<=10 ; i++){
-         if (i>=0 && i<=4){
-             System.out.println("Nota menor que 5 " + i+ " Reprovado");
-            }else if(i>=5 && i<=6){
-             System.out.println("Nota regular  " + i+ " Recuperação");
-            }else if(i>=7 && i<=10) {
-             System.out.println("Nota excelente  " + i + " Parabéns");
-         }
+        Integer soma = n1+n2+n3+n4  ;
+        Float media = (float) (soma /4) ;
 
-         }
+        System.out.println("Sua media é " + media);
+
+        if(media>=0 && media<=4){
+            System.out.println(" Você está Reprovado" );
+        } else if (media>=5 && media<=6) {
+            System.out.println("  Você está de Recuperação" );
+        } else if (media>=7 && media<=10) {
+            System.out.println("Você está Aprovado" );
+        }
+
+
+    }
 
     }
 
 
 
-}
+
