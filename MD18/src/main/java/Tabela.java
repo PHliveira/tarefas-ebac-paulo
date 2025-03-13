3d23a6ad4c68fde92502a9b9e55432bda66ecbef
PHliveira <paulo.criar@gmail.com>
2025-03-13 11:45:35 -0300

    "exercico anotacao 1 parte"

---
+++ MD18/src/main/java/Tabela.java
@@ -0,0 +1,16 @@
+
+
+@Nometabela(value = "Esse é o nome da tabela")
+public class Tabela {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
+
+}
